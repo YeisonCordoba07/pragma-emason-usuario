0e9c29636e879e4eb7f36f3bb2502fd794dfd375
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-09-10 16:21:54 -0500

    first commit

---
+++ src/test/java/com/usuario/pragma/emason/EmasonUsuarioTests.java
@@ -0,0 +1,13 @@
+package com.usuario.pragma.emason;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmasonUsuarioTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
