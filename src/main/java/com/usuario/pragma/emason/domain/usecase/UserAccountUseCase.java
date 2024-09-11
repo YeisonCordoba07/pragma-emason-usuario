dc8c7defb4c826d931fdbb74d2e287ce9990f184
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-09-11 18:57:52 -0500

    feat: add age verification to UserAccountUseCase class

--- src/main/java/com/usuario/pragma/emason/domain/usecase/UserAccountUseCase.java
+++ src/main/java/com/usuario/pragma/emason/domain/usecase/UserAccountUseCase.java
@@ -1,8 +1,13 @@
 package com.usuario.pragma.emason.domain.usecase;
 
+import com.usuario.pragma.emason.domain.Exception.UnderAgeException;
 import com.usuario.pragma.emason.domain.api.IUserAccountService;
 import com.usuario.pragma.emason.domain.model.UserAccount;
 import com.usuario.pragma.emason.domain.spi.IUserAccountPersistence;
+import com.usuario.pragma.emason.domain.util.DomainConstant;
+
+import java.time.LocalDate;
+import java.time.Period;
 
 public class UserAccountUseCase implements IUserAccountService {
 
@@ -11,8 +16,21 @@ public class UserAccountUseCase implements IUserAccountService {
     public UserAccountUseCase(IUserAccountPersistence iUserAccountPersistence) {
         this.iUserAccountPersistence = iUserAccountPersistence;
     }
+
+
+
     @Override
     public void createUserAccount(UserAccount userAccount) {
+        if(!isAdult(userAccount.getBirthDate())){
+            throw new UnderAgeException(DomainConstant.UNDER_AGE_EXCEPTION);
+        }
         iUserAccountPersistence.createUserAccount(userAccount);
+    }
+
+
+    public boolean isAdult(LocalDate birthDate) {
+        LocalDate currentDate = LocalDate.now();
+        Period age = Period.between(birthDate, currentDate);
+        return age.getYears() >= 18;
     }
 }
---
+++ src/main/java/com/usuario/pragma/emason/domain/util/DomainConstant.java
@@ -0,0 +1,5 @@
+package com.usuario.pragma.emason.domain.util;
+
+public class DomainConstant {
+    public static final String UNDER_AGE_EXCEPTION = "El usuario debe ser mayor de edad";
+}
