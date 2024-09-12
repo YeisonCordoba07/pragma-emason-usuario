99f7fc3fb1f3fa8c1e381ca81ff2844267d17263
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-09-11 23:13:42 -0500

    test: add jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("jacoco")
 }
 
 group = "com.usuario.pragma.emason"
@@ -10,6 +11,19 @@ version = "0.0.1-SNAPSHOT"
 java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+jacoco {
+	toolVersion = "0.8.12"
+	reportsDirectory = layout.buildDirectory.dir("customJacocoReportDir")
+}
+
+tasks.jacocoTestReport {
+	reports {
+		xml.required.set(false)
+		csv.required.set(false)
+		html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
 	}
 }
 
@@ -46,6 +60,7 @@ dependencies {
 	testImplementation("org.mockito:mockito-core:4.6.1")
 	testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+
 	//implementation ("org.springframework.boot:spring-boot-starter-security")
 	//implementation ("org.springframework.security:spring-security-core")
 }
@@ -53,3 +68,8 @@ dependencies {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+tasks.test {
+	finalizedBy(tasks.jacocoTestReport) // always generate Jacoco report after tests run
+}
+
