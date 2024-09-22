d125fde9c4a19e82e14445edeaeedf6e633727f1
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-09-22 02:30:09 -0500

    fix: remove unnecesary methods and imports

--- src/main/java/com/usuario/pragma/emason/domain/model/UserAccount.java
+++ src/main/java/com/usuario/pragma/emason/domain/model/UserAccount.java
@@ -2,14 +2,10 @@ package com.usuario.pragma.emason.domain.model;
 
 import com.usuario.pragma.emason.infrastructure.output.entity.EnumRole;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+
+
 
 import java.time.LocalDate;
-import java.util.Collection;
-import java.util.List;
 
 
 
@@ -44,40 +40,6 @@ public class UserAccount  {
     public UserAccount() {
     }
 
-
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
-    }
-
-
-
-
-    public String getUsername() {
-        return email;
-    }
-
-
-
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-
-
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-
-
-    public boolean isEnabled() {
-        return true;
-    }
 
 
     public Long getId() {
